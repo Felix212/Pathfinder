4cf28ddb30d5d130872d9881bc0195a6a48f117d
Felix <felix.groove@googlemail.com>
2019-10-21 17:27:06 +0200

    ryzen tracking example

--- src/app/camera/camera.component.ts
+++ src/app/camera/camera.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, NgZone, ViewChild, ElementRef } from '@angular/core';
 import { registerElement } from "nativescript-angular/element-registry";
-import { AR, ARMaterial,ARPlaneTappedEventData } from "nativescript-ar";
+import { AR, ARMaterial,ARPlaneTappedEventData, ARTrackingImageDetectedEventData, ARLoadedEventData, ARCommonNode } from "nativescript-ar";
 import { Color } from 'tns-core-modules/color/color';
 registerElement("AR", () => require("nativescript-ar").AR);
 
@@ -12,16 +12,61 @@ registerElement("AR", () => require("nativescript-ar").AR);
   moduleId: module.id
 })
 export class CameraComponent {
+    ar: AR;
+    box: Promise<ARCommonNode>;
+    arLoaded(args: ARLoadedEventData) {
+        console.log('loaded');
+
+        this.ar = args.object;
+    }
     foundimage() {
-      console.log('FOUND IMAGE');
-      
+
+        this.box.then(ar => {
+          console.log(ar.getWorldPosition());
+        });
+
+    }
+    createBoxes() {
+        this.ar.addBox({position: {
+            x:1,
+            y: 1,
+            z: 1
+        }, dimensions: {
+          x: 0.15,
+          y: 0.15,
+          z: 0.15
+        }, materials: [new Color("blue")],
+        draggingEnabled: false, });
+        this.ar.addBox({position: {
+            x:0,
+            y: 0,
+            z: 0
+        }, dimensions: {
+          x: 0.15,
+          y: 0.15,
+          z: 0.15
+        }, materials: [new Color("blue")],
+        draggingEnabled: false, });
+    }
+    getCurrentPos() {
+            console.log(this.ar);
+
     }
     constructor() {
       console.log("AR supported? " + AR.isSupported());
     }
-    planeTapped(args: ARPlaneTappedEventData): void {
-      console.log(`Plane tapped at ${args.position.x} y ${args.position.y} z ${args.position.z}`);
-      const ar: AR = args.object;
-      // interact with the 'ar' object here if you like
+
+    imageDetected(args: ARTrackingImageDetectedEventData ) {
+      console.log(args.position, args.imageName);
+       this.box = args.imageTrackingActions.addBox({position: {
+          x: args.position.x,
+          y: args.position.y,
+          z: args.position.z
+      }, dimensions: {
+        x: 0.15,
+        y: 0.15,
+        z: 0.15
+      }, materials: [new Color("blue")],
+      draggingEnabled: false, })
     }
   }
